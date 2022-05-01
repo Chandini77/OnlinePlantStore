33f8a73c7cc3c6b60e7151d8581ff9f5edf80c59
chandini <ychandiniml@gmail.com>
2022-05-01 20:38:55 +0530

    First Commit

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,28 @@
+package com.abc.service;
+public interface UserService {
+	public String login(String userName,String password);
+
+}package com.abc.web;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abc.service.UserService;
+
+@RestController
+@RequestMapping("online")
+public class UserRestController {
+	
+	@Autowired
+	UserService User;
+	 @GetMapping("/home")
+	 public String gethomePage() {
+		return "Welcome to product stores";
+		 
+	 }
+	
+	
+	
+
+}
